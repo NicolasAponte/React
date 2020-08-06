25dd6b7d2d99218ab6ae20af28856c144eb9774c
Nicolas Aponte <nico.aponte98@hotmail.com>
2020-08-05 23:39:24 -0500

    Components 2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { Navbar, NavbarBrand, Form } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />{/*Se adiciona el componente menu*/}
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+
+    super(props);
+
+    this.state = {
+      dishes: DISHES //Se trata como una instanciacion
+    };                  //del DISHES dentro del estado dishes
+  }
+  render(){
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>{/*dishes ahora disponible en el componente menu*/}
+      </div>                             
+    );
+  }
+  
 }
 
 export default App;
