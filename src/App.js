4ec685977f668fd66e1682fbabb5c1a414142482
Nicolas Aponte <nico.aponte98@hotmail.com>
2020-08-11 20:11:02 -0500

    Intro. to Redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 
 class App extends Component {
+  
 
   render(){
     return (
-      <BrowserRouter>{/*Aplicacion ya habilitada para usar Browser Router*/}
-        <div>
-          <Main />{/*dishes ahora disponible en el componente menu*/}
-        </div>   
-      </BrowserRouter>                          
+      <Provider store={store}> 
+        {/*El store ahora disponible en todos los componentes */}
+        <BrowserRouter>{/*Aplicacion ya habilitada para usar Browser Router*/}
+          <div className="App">
+            <Main />{/*dishes ahora disponible en el componente menu*/}
+          </div>   
+        </BrowserRouter>  
+      </Provider>                   
     );
   }
   
